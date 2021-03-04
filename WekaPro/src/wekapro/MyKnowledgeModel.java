155819fbae9202ea2f998909d8bc7a7f9c643a04
MinhChau2910 <79617408+MinhChau2910@users.noreply.github.com>
2021-03-04 15:36:29 +0700

    Bài 05: Lập trình tạo Train set - Test set

--- WekaPro/src/wekapro/MyKnowledgeModel.java
+++ WekaPro/src/wekapro/MyKnowledgeModel.java
@@ -15,6 +15,8 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NominalToBinary;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.filters.unsupervised.instance.Resample;
 
 /**
  *
@@ -25,6 +27,8 @@ class MyKnowledgeModel {
     Instances dataset;
     String [] model_options;
     String [] data_options;
+    Instances trainset;//bai5
+    Instances testset;//bai5
 
     public MyKnowledgeModel() {
     }
@@ -36,8 +40,14 @@ class MyKnowledgeModel {
             String d_opts ) throws Exception {
         this.source = new DataSource(filename);
         this.dataset = source.getDataSet();
-        this.model_options = weka.core.Utils.splitOptions(m_opts);
-        this.model_options = weka.core.Utils.splitOptions(d_opts);
+        
+        if (m_opts != null){
+            this.model_options = weka.core.Utils.splitOptions(m_opts);
+        }
+        if(d_opts != null){
+            this.model_options = weka.core.Utils.splitOptions(d_opts);
+        }
+        
     }
     
     public  Instances removeData(Instances originalDate) throws Exception{
@@ -78,6 +88,26 @@ class MyKnowledgeModel {
         outData.writeBatch();
         System.out.println("Converted");
     }
+    //bai05
+    public Instances divideTrainTest (Instances originalSet, 
+            double percent, boolean isTest) throws Exception{
+        RemovePercentage rp = new RemovePercentage();
+        rp.setPercentage(percent);
+        rp.setInvertSelection(isTest);
+        rp.setInputFormat(originalSet);
+        return Filter.useFilter(originalSet, rp);
+    }
+    
+    public Instances divideTrainTestR(Instances originalSet, 
+            double percent, boolean isTest) throws Exception{
+        Resample rs = new Resample();
+        rs.setNoReplacement(true);
+        rs.setSampleSizePercent(percent);
+        rs.setInvertSelection(isTest);
+        rs.setInputFormat(originalSet);
+        return  Filter.useFilter(originalSet, rs);
+    }
+    
     @Override
     public String toString() {
         return dataset.toSummaryString();
--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -26,12 +26,23 @@ public class WekaPro {
                // "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1", 
                 //"-R 2-3");
         //model.mineAssociationRules();
-        MyFPGrowthModel model = new MyFPGrowthModel(
-                "C:\\Program Files\\Weka-3-9\\data\\weather.nominal.arff", 
-                "-P 2 -I -1 -N 10 -T 0 -C 0.8 -D 0.05 -U 1.0 -M 0.2",
-                "-N-R first-last");
-        model.mineAssociationRules();
+        
+        
+        //MyFPGrowthModel model = new MyFPGrowthModel(
+                //"C:\\Program Files\\Weka-3-9\\data\\weather.nominal.arff", 
+                //"-P 2 -I -1 -N 10 -T 0 -C 0.8 -D 0.05 -U 1.0 -M 0.2",
+                //"-N-R first-last");
+        //model.mineAssociationRules();
+        //System.out.println(model);
+        
+        
+        //bài 05
+        MyKnowledgeModel model = new MyKnowledgeModel( "C:\\Program Files\\Weka-3-9\\data\\iris.arff",null,null);
+        model.trainset = model.divideTrainTestR(model.dataset,80,false);
+        model.testset = model.divideTrainTestR(model.dataset,80,true);
         System.out.println(model);
+        System.out.println(model.trainset.toSummaryString());
+        System.out.println(model.testset.toSummaryString());
     }
     
 }
